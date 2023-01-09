c7cd95da886737bdcf2d3c55f0c04e0e349855f6
AATIR Mohamed <105876846+AatirMed@users.noreply.github.com>
2023-01-09 13:04:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,140 +1,95 @@
-import React, { useState,  Fragment } from "react";
-import Congratulations from "./Congratulations";
+import React, { useState, useEffect } from "react";
 import './App.css';
-import { useEffect } from "react";
 
 const App = () => {
-  const styleSpace = { marginRight: '25px' };
-  //countries
-  const countries = [
-    'Maroc',
-    "Afghanistan",
-    "Albania",
-    "Algeria",
-    "Andorra",
-    "Angola",
-    "Antigua",
-    "Argentina",
-    "Armenia",
-    "Australia",
-    "Austria",
-    "Azerbaijan",
-    "Bahamas",
-    "Bahrain",
-    "Bangladesh",
-    "Barbados",
-    "Belarus",
-    "Belgium",
-    "Belize",
-    "Benin",
-    "Bhutan"
-  ];
+  //Pizza
+  const [Pizza, setPizza] = useState(
+    [
+      { id: 0, name: 'Pizza 4 fromages', prix: 80, qte: 1 },
+      { id: 10, name: 'Pizza Herbo', prix: 75, qte: 0 },
+      { id: 20, name: 'Pizza viande hachee', prix: 100, qte: 0 },
+      { id: 30, name: 'Pizza fuits de mer', prix: 120, qte: 0 }
+    ]
+  )
 
-  //data
-  const [data, setData] = useState(
-    {
-      Nom: '',
-      prenom: '',
-      Age: 0,
-      Sexe: '',
-      Nationnalite: '',
-      Pays: '0',
-      Interets: [],
-    }
-  );
-
-  const [reverse, setR] = useState(true);
-
-  const handChangeInput = (event) => {
-    const { value, name } = event.target;
-    setData({ ...data, [name]: value })
+  //data 
+  const [data, setData] = useState({ name: '', address: '', typePay: '', Nbr: '' })
+  const [radioDisplay, setradioDisplay] = useState(false);
+  const [total, setT] = useState(80);
+  const [Message, setMessage] = useState('');
+  // Change Qte Produit
+  const HandChangeQteProduit = (event) => {
+    const { name, value } = event.target;
+    setPizza([...Pizza.map(ele => ele.id === parseInt(name) ? { ...ele, qte: value } : { ...ele })])
   }
 
-  const handChangeInterets = (event) => {
-    const { value, checked } = event.target
-    if (checked) {
-      setData({ ...data, Interets: [...data.Interets, value] })
-    } else {
-      setData({ ...data, Interets: [...data.Interets.filter(ele => ele !== value)] })
+  //Name && Address && typeCart ...
+  const HandChangeInput = (event) => {
+    const { name, value } = event.target;
+    setData({ ...data, [name]: value });
+  }
+  //Cart
+  useEffect(() => {
+    data.typePay === 'cheque' ? setradioDisplay(true) : setradioDisplay(false);
+  }, [data])
+  
+  //total
+  useEffect(() => {
+    setT(Pizza.reduce((T, obj) => T + (obj.qte !== '' ? parseInt(obj.qte) * obj.prix : 0), 0));
+  }, [Pizza])
+
+  //valid
+  const valid = () => {
+    var c = 0;
+    if (Pizza.filter(ele => ele.qte !== '' && parseInt(ele.qte) !== 0).length === 0) { c = +1 };
+    if (data.typePay === '') { c += 1 }
+    if (data.name === '') { c += 1 }
+    if (data.address === '') { c += 1 };
+    if (data.typePay === 'carte' && data.Nbr === '') { c += 1 };
+    if (c === 0) { 
+      setMessage('Merci de votre visite, le montant total de votre commande est :') }
+    else {
+      setMessage('entry value is not found')
     }
   }
 
-  const Validation = () => {
-    var i = 0;
-    if (!/^[A-Za-z]{3,}$/.test(data.Nom)) {
-      i = i + 1;
-    } else if (!/^[A-Za-z]{3,}$/.test(data.prenom)) {
-      i = i + 1;
-    } else if (parseInt(data.Age) < 18) {
-      i = i + 1;
-    } else if (data.Interets.length === 0) {
-      i = i + 1;
-    } else if (data.Pays === '') {
-      i = i + 1;
-    } else if (data.Sexe === '' && data.Nationnalite === '') {
-      i = i + 1;
-    }
 
-    if (i === 0) {
-      setR(false);
-    }
-  }
 
-  const createNew = ()=>{
-    setData({
-      Nom: '',
-      prenom: '',
-      Age: 0,
-      Sexe: '',
-      Nationnalite: '',
-      Pays: '0',
-      Interets: [],
-    })
-    setR(true)
-    
-  }
   return (
-    <Fragment>
-      {reverse ?
-        <div className="main">
-          <span>Nom : </span> <input style={styleSpace} type='text' name='Nom' onChange={handChangeInput} />
-          <span>prenom : </span> <input type='text' name='prenom' onChange={handChangeInput} />
-          <br /><br />
+    <div className="main">
+      <h2>Vente Pizza</h2>
+      <span>Nom du client : </span><input type='text' onChange={HandChangeInput} name='name' placeholder="Name..." /><br /><br />
+      <span>Address : </span><input type='text' onChange={HandChangeInput} name='address' placeholder="Address..." /><br /><br />
 
-          <span>Age : </span> <input type='number' name='Age' onChange={handChangeInput} />
-          <br /><br />
+      <table>
+        <thead>
+          <tr>
+            <th>Nom du produit</th>
+            <th>Prix unitaire en DH</th>
+            <th>Quantite</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Pizza.map(ele =>
+            <tr key={ele.id}>
+              <th>{ele.name}</th>
+              <th>{ele.prix}</th>
+              <th><input type='number' name={ele.id} defaultValue={ele.qte} placeholder="Qte..." onChange={HandChangeQteProduit} /></th>
+            </tr>
+          )}
+        </tbody>
+      </table>
 
-          <span style={styleSpace}>Sexe : </span>
-          <span>Homme</span><input style={styleSpace} type='radio' value='Homme' name='Sexe' onChange={handChangeInput} />
-          <span>Femme</span><input type='radio' value='Femme' name='Sexe' onChange={handChangeInput} />
-          <br /><br />
-
-          <span style={styleSpace}>Nationnalite : </span>
-          <span>Marocaine</span><input style={styleSpace} type='radio' value='Marocaine' name='Nationnalite' onChange={handChangeInput} />
-          <span>Etrangere</span><input type='radio' value='Etrangere' name='Nationnalite' onChange={handChangeInput} />
-          <br /><br />
-          <span style={styleSpace}>Pays : </span>
-          <select name="Pays" onClick={handChangeInput}>
-            <option value='0'>Pays</option>
-            {countries.map(country => <option key={country} value={country}>{country}</option>)}
-          </select>
-          <br /><br />
-          <span>Interets : </span>
-          <ul>
-            <li>
-              <span>Lecture</span><input type='checkbox'  value='Lecture' style={styleSpace} onChange={handChangeInterets} />
-              <span>Cinema</span><input type='checkbox'  value='Cinema' style={styleSpace} onChange={handChangeInterets} />
-              <span>Sports</span><input type='checkbox'  value='Sports' style={styleSpace} onChange={handChangeInterets} />
-            </li>
-            <li>
-              <span>Theatre</span><input type='checkbox'  value='Theatre' style={styleSpace} onChange={handChangeInterets} />
-              <span>Voyage</span><input type='checkbox'  value='Voyage' style={styleSpace} onChange={handChangeInterets} />
-              <span>Sciences</span><input type='checkbox'  value='Sciences' style={styleSpace} onChange={handChangeInterets} />
-            </li>
-          </ul>
-          <button onClick={Validation}>Affiche</button>
-        </div> : <Congratulations changeValue={createNew} />}
-    </Fragment>
+      <div>
+        <h3>Paiement par : </h3>
+        <input type='radio' name='typePay' value='carte' onChange={HandChangeInput} /><span>carte bancaire</span>
+        <input type='radio' name='typePay' value='cheque' onChange={HandChangeInput} /><span>cheque</span><br /><br />
+        <span>Number de la carte bancaire : </span> <input type='number' name='Nbr' onChange={HandChangeInput} disabled={radioDisplay} />
+        <br /><br />
+        <button onClick={valid}>Envoyer</button><br /><br />
+        <span >{Message} {Message==='Merci de votre visite, le montant total de votre commande est :'?total:null}</span>
+      </div>
+    </div>
   );
 };
 
