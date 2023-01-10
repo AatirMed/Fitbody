357d2afaf654d171ee9acf996f2ccb46b4f7c475
AATIR Mohamed <105876846+AatirMed@users.noreply.github.com>
2023-01-10 13:56:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,93 +2,69 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 
 const App = () => {
-  //Pizza
-  const [Pizza, setPizza] = useState(
-    [
-      { id: 0, name: 'Pizza 4 fromages', prix: 80, qte: 1 },
-      { id: 10, name: 'Pizza Herbo', prix: 75, qte: 0 },
-      { id: 20, name: 'Pizza viande hachee', prix: 100, qte: 0 },
-      { id: 30, name: 'Pizza fuits de mer', prix: 120, qte: 0 }
-    ]
-  )
-
-  //data 
-  const [data, setData] = useState({ name: '', address: '', typePay: '', Nbr: '' })
-  const [radioDisplay, setradioDisplay] = useState(false);
-  const [total, setT] = useState(80);
+  //data
+  const [data, setData] = useState([
+    { id: 1, name: 'Pain', qte: 0, check: true, uni: "grammes" },
+    { id: 2, name: 'Viande', qte: 0, check: false, uni: "grammes" },
+    { id: 3, name: 'Legumes', qte: 0, check: false, uni: "grammes" },
+    { id: 4, name: 'Banane', qte: 0, check: false, uni: "unites" },
+    { id: 5, name: 'Pomme', qte: 0, check: false, uni: "unites" },
+    { id: 6, name: 'Yaourt', qte: 0, check: false, uni: "unites" },
+  ]);
   const [Message, setMessage] = useState('');
-  // Change Qte Produit
-  const HandChangeQteProduit = (event) => {
+
+  //change value Qte
+  const HandChangeValue = (event) => {
     const { name, value } = event.target;
-    setPizza([...Pizza.map(ele => ele.id === parseInt(name) ? { ...ele, qte: value } : { ...ele })])
+    setData([...data.map(ele => ele.name === name ? { ...ele, qte: parseInt(value) } : { ...ele })])
   }
 
-  //Name && Address && typeCart ...
-  const HandChangeInput = (event) => {
-    const { name, value } = event.target;
-    setData({ ...data, [name]: value });
+  //checkbox
+  const handChangeCheck = (event) => {
+    const { value } = event.target;
+    setData([...data.map(obj => obj.id === parseInt(value) ? { ...obj, check: !obj.check } : { ...obj })]);
+    // console.log(typeof value)
   }
-  //Cart
-  useEffect(() => {
-    data.typePay === 'cheque' ? setradioDisplay(true) : setradioDisplay(false);
-  }, [data])
-  
-  //total
-  useEffect(() => {
-    setT(Pizza.reduce((T, obj) => T + (obj.qte !== '' ? parseInt(obj.qte) * obj.prix : 0), 0));
-  }, [Pizza])
 
-  //valid
+  //validation
   const valid = () => {
-    var c = 0;
-    if (Pizza.filter(ele => ele.qte !== '' && parseInt(ele.qte) !== 0).length === 0) { c = +1 };
-    if (data.typePay === '') { c += 1 }
-    if (data.name === '') { c += 1 }
-    if (data.address === '') { c += 1 };
-    if (data.typePay === 'carte' && data.Nbr === '') { c += 1 };
-    if (c === 0) { 
-      setMessage('Merci de votre visite, le montant total de votre commande est :') }
-    else {
-      setMessage('entry value is not found')
+    if (data.filter(ele => ele.check === true).length !== 0) {
+      var kilo = 0;
+      if (data[0].name === 'Pain' && data[0].check === true) kilo += (data[0].qte * 2.75);
+      if (data[1].name === 'Viande' && data[1].check === true) kilo += (data[1].qte * 1.8);
+      if (data[2].name === 'Legumes' && data[2].check === true) kilo += (data[2].qte * 0.4);
+      if (data[3].name === 'Banane' && data[3].check === true) kilo += (data[3].qte * 116);
+      if (data[4].name === 'Pomme' && data[4].check === true) kilo += (data[4].qte * 80);
+      if (data[5].name === 'Yaourt' && data[5].check === true) kilo += (data[5].qte * 140);
+      setMessage('La valeur energetique de votre repas est  ' + kilo + '  kilo calories')
+    } else {
+      setMessage('au moins cocher une case')
     }
   }
-
-
+  
+  //Message 
+  useEffect(() => {
+    setMessage('')
+  }, [data])
 
   return (
     <div className="main">
-      <h2>Vente Pizza</h2>
-      <span>Nom du client : </span><input type='text' onChange={HandChangeInput} name='name' placeholder="Name..." /><br /><br />
-      <span>Address : </span><input type='text' onChange={HandChangeInput} name='address' placeholder="Address..." /><br /><br />
+      <h1>Calcul valeur energetique d'un repas</h1>
+      <h4>Choisir les aliments qui conposent votre repas : </h4>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Nom du produit</th>
-            <th>Prix unitaire en DH</th>
-            <th>Quantite</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Pizza.map(ele =>
-            <tr key={ele.id}>
-              <th>{ele.name}</th>
-              <th>{ele.prix}</th>
-              <th><input type='number' name={ele.id} defaultValue={ele.qte} placeholder="Qte..." onChange={HandChangeQteProduit} /></th>
-            </tr>
-          )}
-        </tbody>
-      </table>
-
-      <div>
-        <h3>Paiement par : </h3>
-        <input type='radio' name='typePay' value='carte' onChange={HandChangeInput} /><span>carte bancaire</span>
-        <input type='radio' name='typePay' value='cheque' onChange={HandChangeInput} /><span>cheque</span><br /><br />
-        <span>Number de la carte bancaire : </span> <input type='number' name='Nbr' onChange={HandChangeInput} disabled={radioDisplay} />
-        <br /><br />
-        <button onClick={valid}>Envoyer</button><br /><br />
-        <span >{Message} {Message==='Merci de votre visite, le montant total de votre commande est :'?total:null}</span>
-      </div>
+      {
+        data.map((ele, index) => {
+          return (
+            <div key={index}>
+              <input type='checkbox' defaultChecked={ele.check} value={ele.id} onChange={handChangeCheck} />
+              <span>{ele.name} : </span>
+              <input type='number' name={ele.name} disabled={!ele.check} defaultValue={ele.qte} onChange={HandChangeValue} /> {ele.uni} <br /><br />
+            </div>
+          )
+        })
+      }
+      <button style={{ marginLeft: '120px' }} onClick={valid}>calculer</button><br /><br />
+      <span className={Message === 'au moins cocher une case' ? 'red' : 'green'}> {Message}</span>
     </div>
   );
 };
