b8f68ea8a1302bf49c016228abda87133f85d286
AatirMed1 <sporthd1973@gmail.com>
2023-04-02 18:02:28 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
-import './All/css/App.css';
+import { useEffect } from "react";
+import Aos from "aos";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Hero from "./components/UI/Hero";
+import Exercises from "./components/UI/Exercises";
+import Start from "./components/UI/Start";
+import Pricing from "./components/UI/Pricing";
+import Testimonials from "./components/UI/Testimonials";
+import Footer from "./components/UI/Footer";
 
-import Dashbord from "./All/dashbord/Dashbord";
+function App() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
 
-const App = () => {
-  return (<Dashbord />);
-};
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Exercises />
+      <Start />
+      <Pricing />
+      <Testimonials />
+      <Footer />
+    </>
+  );
+}
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
-import reportWebVitals from './reportWebVitals';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import store from './All/Store/store';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "remixicon/fonts/remixicon.css";
+import "aos/dist/aos.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
-    <BrowserRouter>
     <App />
-    </BrowserRouter>
-    
-    </Provider>
-    
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
